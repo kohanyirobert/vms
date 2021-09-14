9a6d9387bd36d5fa35e179c3f8def9e3fd2739bc
Kohányi Róbert <kohanyi.robert@gmail.com>
2021-09-14 19:02:13 +0200

    Fix mininet install script

--- scripts/mininet.sh
+++ scripts/mininet.sh
@@ -4,7 +4,7 @@ systemctl stop openvswitch-testcontroller
 systemctl disable openvswitch-testcontroller
 systemctl enable openvswitch-switch
 systemctl start openvswitch-switch
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+curl https://bootstrap.pypa.io/pip/2.7/get-pip.py -o get-pip.py
 python get-pip.py
 pip install ipaddress
 rm -rf get-pip.py
